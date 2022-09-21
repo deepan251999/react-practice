fea90e28f94fa457d893082b293dc3f6c79cf1d4
deepan251999 <deepan251999@gmail,com>
2022-09-21 21:41:07 +0530

    react-practice.

---
+++ myfirstproject/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function App() {
+  return (
+    
+    <div className='header'>
+      <div className='containner'>
+      <div className='item'> 
+     
+         <h2>Repellat Nihil</h2>
+         <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+       </div>
+       <div className='item'> 
+         <h2>Repellat Nihil</h2>
+         <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+       </div>
+       <div className='item'> 
+         <h2>Repellat Nihil</h2>
+         <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+       </div>
+       <div className='item'> 
+         <h2>Repellat Nihil</h2>
+         <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+       </div>
+       <div className='item'> 
+         <h2>Repellat Nihil</h2>
+         <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+       </div>
+       <div className='item'> 
+         <h2>Repellat Nihil</h2>
+         <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+       </div>
+       <div className='item'> 
+         <h2>Repellat Nihil</h2>
+         <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+       </div>
+       <div className='item'> 
+         <h2>Repellat Nihil</h2>
+         <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+       </div>
+       </div>
+    </div>
+  )
+};
+
+export default App;
